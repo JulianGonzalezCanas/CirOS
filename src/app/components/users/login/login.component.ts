f74da9c82755f88b1792b42be8b03432935157b6
JulianGonzalezCanas <alumno24.gonzalez.julian@ipm.edu.ar>
2024-08-20 13:11:59 -0300

    modificaciones en verificacion de usuario

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,13 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
+import { NgClass, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule, ReactiveFormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -18,16 +19,31 @@ export class LoginComponent {
   constructor(private authService: AuthService, private formBuilder: FormBuilder, private router: Router) {
     this.service = authService;
     this.formLog = this.formBuilder.group({
-      mail: '',
-      contra: ''
+      mail: ['', [Validators.required, Validators.email]],
+      contra: ['', Validators.required]
     });
   }
 
 log(){
+
+  if (this.formLog.valid) {
+    // Obtén los valores del formulario
+    const { mail, contra } = this.formLog.value;
+    console.log(mail, contra);
+  }
+  else {
+    // Muestra errores si el formulario no es válido
+    console.log('Formulario inválido');
+    this.formLog.markAllAsTouched();
+  }
+  
   this.service.logUser(this.formLog.get('mail')?.value, this.formLog.get('contra')?.value).subscribe((res: any) => {
     localStorage.setItem('token', res.token);
     this.router.navigateByUrl('/');
   });
 }
+
+
+
 
 }
