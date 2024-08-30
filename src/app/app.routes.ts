00813b753f83dfaca010f7eac2a413e6c0ca810a
JulianGonzalezCanas <alumno24.gonzalez.julian@ipm.edu.ar>
2024-08-29 21:52:12 -0300

    modificaciones en guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/users/login/login.component';
 import { SigninComponent } from './components/users/signin/signin.component';
 import { AuthGuard } from './services/auth.guard';
-import { PerfilComponent } from './components/profile/profile.component';
+import { PerfilComponent } from './components/users/profile/profile.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
     {path:'login', component: LoginComponent},
     {path:'register', component: SigninComponent},
     {path:'profile', component: PerfilComponent},
-
-   /*{
+    
+    {
         path:"",
         canActivate: [AuthGuard],
         children: [
-            {path:'profile', component: ProfileComponent},
-        ]
-    }*/
+            {path:'profile', component: PerfilComponent},
+        ],
+    },
 ];
--- src/app/components/users/profile/profile.component.ts
+++ src/app/components/users/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { IUser } from '../../models/user.model';
-import { UserService } from '../../services/user.service';
+import { IUser } from '../../../models/user.model';
+import { UserService } from '../../../services/user.service';
 import { CommonModule } from '@angular/common';
 
 
