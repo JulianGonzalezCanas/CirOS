d5d08348c0f97aeca1620df94d2e05da6ec3d5de
JulianGonzalezCanas <alumno24.gonzalez.julian@ipm.edu.ar>
2024-10-23 16:06:58 -0300

    solucione un problema con el modelo

---
+++ src/app/components/products/c-phone/c-phone.component.ts
@@ -0,0 +1,28 @@
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-c-phone',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './c-phone.component.html',
+  styleUrl: './c-phone.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CPhoneComponent implements AfterViewInit {
+
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const script = this.renderer.createElement('script');
+      script.type = 'module';
+      script.src = 'https://unpkg.com/@splinetool/viewer@1.9.32/build/spline-viewer.js';
+      this.renderer.appendChild(document.body, script);
+    }
+  }
+
+}
